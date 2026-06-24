d2d8d09d4ae94fbb864ad855cd7be1b130f67f75
Vamsi Krishna Reddy <149224432+vamsi17112005@users.noreply.github.com>
2026-06-24 11:51:37 +0530

    Delete app/test_pdf.py

--- app/test_pdf.py
+++
@@ -1,7 +0,0 @@
-from services.extractors.pdf_extractor import extract_pdf
-
-doc = extract_pdf(
-    r"C:\Users\vamsi\OneDrive\Desktop\my_intern_placement_prep\ai-search-backend\test_files\offer_letter.pdf"
-)
-
-print(doc.export_to_markdown())
