a6d0245ea6e86d53934c4ec438950f69ca6b2756
Vamsi Krishna Reddy <149224432+vamsi17112005@users.noreply.github.com>
2026-06-24 11:51:15 +0530

    Delete app/test_db.py

--- app/test_db.py
+++
@@ -1,8 +0,0 @@
-from database import engine
-from sqlalchemy import text
-
-with engine.connect() as conn:
-    result = conn.execute(text("SELECT current_database();"))
-
-    for row in result:
-        print(row)
